ce7eb7861e5ec17f94d299372cc39a010df86ce0
Dorian Roche Philis <dodolkako@yahoo.fr>
2020-02-17 19:41:43 +0100

    add misses dot

--- app/models/monument.rb
+++ app/models/monument.rb
@@ -3,7 +3,7 @@ class Monument < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
   validates :price, presence: true
-  validates :description presence: true
+  validates :description, presence: true
   validates :city, presence: true
   validates :name, uniqueness: true
 end
