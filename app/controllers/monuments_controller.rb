786a7b23c90a342621a99f5a3bb5178886dff080
Ambre Margerit <ambremgt@gmail.com>
2020-02-18 14:20:56 +0100

    debug page index

--- app/controllers/monuments_controller.rb
+++ app/controllers/monuments_controller.rb
@@ -1,63 +1,64 @@
 class MonumentsController < ApplicationController
-  skip_before_action :authenticate_user! only: [:index, :show, :search]
+  skip_before_action :authenticate_user!, only: [:index, :show, :search]
   before_action :set_monument, only: [:show, :edit, :update, :destroy]
-end
 
-def index
-  if params[:query].present?
-    @monuments = Monument.where("name ILIKE ?", "%#{params[:query]}%")
-  else
-    @monuments = Monument.all
+  def index
+    @monuments = policy_scope(Monument).order(created_at: :desc)
+    if params[:query].present?
+      @monuments = Monument.where("name ILIKE ?", "%#{params[:query]}%")
+    else
+      @monuments = Monument.all
+    end
   end
-end
 
-def new
-  @monument = Monument.new
-  authorize @monument
-end
-
-def create
-  @monument = Monument.new(monument_params)
-  @monument.user = current_user
-  authorize @monument
-  if @monument.save
-    redirect_to monument_path(@monument)
-  else
-    render :new
+  def new
+    @monument = Monument.new
+    authorize @monument
   end
-end
 
-def show
-end
-
-def edit
-  authorize @monument
-end
-
-def update
-  authorize @monument
-  if @monument.update(monument_params)
-    redirect_to monument_path(@monument)
-  else
-    render :edit
+  def create
+    @monument = Monument.new(monument_params)
+    @monument.user = current_user
+    authorize @monument
+    if @monument.save
+      redirect_to monument_path(@monument)
+    else
+      render :new
+    end
   end
-end
 
-def destroy
-  authorize @monument
-  @monument.destroy
-  redirect_to root_path
-  # redirect to dashboard
-end
+  def show
+  end
 
-private
+  def edit
+    authorize @monument
+  end
 
-def set_monument
-  @monument = Monument.find(params[:id])
-end
+  def update
+    authorize @monument
+    if @monument.update(monument_params)
+      redirect_to monument_path(@monument)
+    else
+      render :edit
+    end
+  end
 
-def monument_params
-  params.require(:monument).permit(:user, :address, :name, :price, :description, :city)
+  def destroy
+    authorize @monument
+    @monument.destroy
+    redirect_to root_path
+    # redirect to dashboard
+  end
+
+  private
+
+  def set_monument
+    @monument = Monument.find(params[:id])
+  end
+
+  def monument_params
+    params.require(:monument).permit(:user, :address, :name, :price, :description, :city)
+  end
 end
 
 
