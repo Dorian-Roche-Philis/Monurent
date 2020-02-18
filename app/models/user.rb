54fdff59e96d9993760188cbe8dd57424a45efe8
Lital Levin <lital.levin@gmail.com>
2020-02-18 11:40:19 +0100

    change phone number from string to integer

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
-  # validates :address, presence: true
+  validates :address, presence: true
 
 
   devise :database_authenticatable, :registerable,
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_18_095418) do
+ActiveRecord::Schema.define(version: 2020_02_18_103805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2020_02_18_095418) do
     t.string "first_name"
     t.string "last_name"
     t.string "address"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
