b0e40bf096e6cd46c52395c4ee008c65e91727f5
Dorian Roche Philis <dodolkako@yahoo.fr>
2020-02-18 11:59:05 +0100

    add parse and create 100 monuments with 5 users, scrapping "
    "

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,47 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+require 'open-uri'
+require 'nokogiri'
+
+
+url = "https://designlike.com/100-most-famous-landmarks-around-the-world/"
+array = []
+html_file = open(url).read
+html_doc = Nokogiri::HTML(html_file)
+ html_doc.search('strong').each do |title|
+  arr = title.text.strip.split('. ')
+  if arr[1] != nil
+    array << arr[1]
+  end
+end
+monuments = []
+ array.each do |x|
+  if x[1] != nil
+  monuments << x.split(" in ")
+end
+
+ end
+  anne =User.create!(first_name: "Anne", last_name: "Hidalgo", email: "anne@paris.com", phone_number: "06 75 00 07 50", address: "Notre de dame 75000 Paris")
+  ben = User.create!(first_name: "Benjamin", last_name: "Netanyahu", email: "benjamin@tel-aviv.com", phone_number: "9726 61 00 061 99", address: "Tayelet, Hayarkon, Tel Aviv")
+  donald = User.create!(first_name: "Donald", last_name: "Trump", email: "donald@washington.com", phone_number: "01 23 45 67 89", address: "Trump Tower, 725 5th Ave, New York, NY 10022")
+ vlad = User.create!(first_name: "Vladimir", last_name: "Putin", email: "vladimir@moscow.com", phone_number: "0007 007 007", address: "The Moscow Kremlin, Moscow, Russia, 103073")
+ grigri = User.create!(first_name: "Benjamin", last_name: "Griveaux", email: "benjamin@film-maker.com", phone_number: "06 96 96 75 00", address: "Grand Pigalle Hotêl, 29 Rue Victor Massé 75009 Paris")
+ price = (50...500).to_a
+ tab = [anne, ben, donald, vlad, grigri]
+ description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+monuments.each do |monument|
+    Monument.create!(name: monument[0], city: monument[1], price: price.sample, description: description, user_id: tab.sample )
+end
+p Monument.all
+
+   #array << title.text.strip.split(', ')
+
+
+  #puts element.text.strip
+  #puts element.attribute('href').value
+  #p array
+
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
