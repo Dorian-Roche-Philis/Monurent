3c098712198b87964706bc4a2ebf3970881deed3
Ambre Margerit <ambremgt@gmail.com>
2020-02-19 11:35:13 +0100

    text ok

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -3,7 +3,7 @@ import Typed from 'typed.js';
 const loadDynamicBannerText = () => {
   if (document.getElementById("banner-typed-text")) {
     new Typed('#banner-typed-text', {
-      strings: ["Create your own cocktail", "Mojito", "Caipirina", "And much more!"],
+      strings: ["MONURENT", "Rent a monument", "For you events"],
       typeSpeed: 50,
       loop: true
     });
